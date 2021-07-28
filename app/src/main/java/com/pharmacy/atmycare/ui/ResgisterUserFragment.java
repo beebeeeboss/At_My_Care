57c8922be66ff6395601fc4e48108a4bcd7887c4
ParamSaini <74251396+param-18@users.noreply.github.com>
2021-07-28 22:08:42 +0530

    Setup the Project according to nav-graph

---
+++ app/src/main/java/com/pharmacy/atmycare/ui/ResgisterUserFragment.java
@@ -0,0 +1,22 @@
+package com.pharmacy.atmycare.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pharmacy.atmycare.R;
+
+
+public class ResgisterUserFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_resgister_user, container, false);
+    }
+}
---
+++ app/src/main/java/com/pharmacy/atmycare/ui/StarterActivity.java
@@ -0,0 +1,19 @@
+package com.pharmacy.atmycare.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavGraph;
+import androidx.navigation.NavHostController;
+
+import android.os.Bundle;
+
+import com.pharmacy.atmycare.R;
+
+public class StarterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starter);
+
+    }
+}
