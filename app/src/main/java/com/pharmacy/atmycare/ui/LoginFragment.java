cf4b91ca3d550f70f7e618faef687730c876e358
ParamSaini <74251396+param-18@users.noreply.github.com>
2021-08-02 16:49:35 +0530

    OTP receiver and button designs changed

--- app/src/main/java/com/pharmacy/atmycare/ui/LoginFragment.java
+++ app/src/main/java/com/pharmacy/atmycare/ui/LoginFragment.java
@@ -1,11 +1,13 @@
 package com.pharmacy.atmycare.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,14 +15,22 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.mukesh.OnOtpCompletionListener;
+import com.mukesh.OtpView;
 import com.pharmacy.atmycare.R;
 import com.pharmacy.atmycare.databinding.FragmentLoginBinding;
+
+import in.aabhasjindal.otptextview.OTPListener;
+import in.aabhasjindal.otptextview.OtpTextView;
 
 
 public class LoginFragment extends Fragment {
 
     private FragmentLoginBinding binding;
     private String userType;
+    private  OtpTextView otpView;
     // use this binding for find view by id purpose
     // like textview as binding.idnamedefinedinresourcefile
     @Override
@@ -37,7 +47,7 @@ public class LoginFragment extends Fragment {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext() , R.array.user_type, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         binding.spLoginType.setAdapter(adapter);
-
+         otpView = binding.ovReceivedOtp;
         binding.spLoginType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -47,6 +57,57 @@ public class LoginFragment extends Fragment {
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+
+        binding.btnSendOTP.setOnClickListener(v->
+        {
+            if(binding.etPhoneNumber.getText().toString().length() < 10)
+            {
+               binding.tilPhoneNumber.setError("Please Fill Valid Mobile Number");
+            }
+            else
+            {
+                binding.etPhoneNumber.setEnabled(false);
+                binding.tilPhoneNumber.setVisibility(View.GONE);
+                binding.btnSendOTP.setVisibility(View.GONE);
+
+                binding.tvResendOTP.setVisibility(View.VISIBLE);
+                binding.tvChangeNumber.setVisibility(View.VISIBLE);
+            }
+        });
+
+        otpView.setOtpListener(new OTPListener() {
+            @Override
+            public void onInteractionListener() {
+
+            }
+
+            @Override
+            public void onOTPComplete(String otp) {
+                if (otpView.getOTP().equals("123456")) {
+                    otpView.showSuccess();
+                } else {
+                    otpView.showError();
+                }
+            }
+        });
+
+        binding.tvChangeNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.etPhoneNumber.setEnabled(true);
+                binding.tilPhoneNumber.setVisibility(View.VISIBLE);
+                binding.btnSendOTP.setVisibility(View.VISIBLE);
+                binding.tvResendOTP.setVisibility(View.GONE);
+                binding.tvChangeNumber.setVisibility(View.GONE);
+            }
+        });
+
+        binding.tvResendOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                otpView.setOTP("");
             }
         });
     }
