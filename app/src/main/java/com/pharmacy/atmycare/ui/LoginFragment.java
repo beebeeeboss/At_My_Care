eb215bbe0088d6e42f94230048714a83c768fe4e
ParamSaini <74251396+param-18@users.noreply.github.com>
2021-08-05 19:04:11 +0530

    Temp Connectivity of Login Fragment with Registration Fragment Done

--- app/src/main/java/com/pharmacy/atmycare/ui/LoginFragment.java
+++ app/src/main/java/com/pharmacy/atmycare/ui/LoginFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.os.CountDownTimer;
 import android.view.LayoutInflater;
@@ -13,8 +14,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.mukesh.OnOtpCompletionListener;
@@ -29,7 +32,6 @@ import in.aabhasjindal.otptextview.OtpTextView;
 public class LoginFragment extends Fragment {
 
     private FragmentLoginBinding binding;
-    private String userType;
     private  OtpTextView otpView;
     // use this binding for find view by id purpose
     // like textview as binding.idnamedefinedinresourcefile
@@ -48,32 +50,24 @@ public class LoginFragment extends Fragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         binding.spLoginType.setAdapter(adapter);
          otpView = binding.ovReceivedOtp;
-        binding.spLoginType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
         binding.btnSendOTP.setOnClickListener(v->
         {
-            if(binding.etPhoneNumber.getText().toString().length() < 10)
+            if(binding.spLoginType.getSelectedItemPosition() == 0)
             {
-               binding.tilPhoneNumber.setError("Please Fill Valid Mobile Number");
+                Snackbar.make(getView(),"Please Select Your Role First.",Snackbar.LENGTH_LONG).show();
             }
-            else
-            {
-                binding.etPhoneNumber.setEnabled(false);
-                binding.tilPhoneNumber.setVisibility(View.GONE);
-                binding.btnSendOTP.setVisibility(View.GONE);
-                binding.llOtp.setVisibility(View.VISIBLE);
-                binding.tvResendOTP.setVisibility(View.VISIBLE);
-                binding.tvChangeNumber.setVisibility(View.VISIBLE);
+            else {
+                if (binding.etPhoneNumber.getText().toString().length() < 10) {
+                    binding.tilPhoneNumber.setError("Please Fill Valid Mobile Number");
+                } else {
+                    binding.etPhoneNumber.setEnabled(false);
+                    binding.tilPhoneNumber.setVisibility(View.GONE);
+                    binding.btnSendOTP.setVisibility(View.GONE);
+                    binding.llOtp.setVisibility(View.VISIBLE);
+                    binding.tvResendOTP.setVisibility(View.VISIBLE);
+                    binding.tvChangeNumber.setVisibility(View.VISIBLE);
+
+                }
             }
         });
 
@@ -87,6 +81,18 @@ public class LoginFragment extends Fragment {
             public void onOTPComplete(String otp) {
                 if (otpView.getOTP().equals("123456")) {
                     otpView.showSuccess();
+                    switch (binding.spLoginType.getSelectedItemPosition()) {
+                        case 1:
+                            //go to admin
+                            break;
+                        case 2:
+                            //go to ATMx Register Form or Dashboard according to database
+                            break;
+                        case 3:
+                            //go to User Register Form or Dashboard according to database
+                            Navigation.findNavController(getView()).navigate(LoginFragmentDirections.actionLoginFragmentToResgisterUserFragment());
+                            break;
+                    }
                 } else {
                     otpView.showError();
                 }
