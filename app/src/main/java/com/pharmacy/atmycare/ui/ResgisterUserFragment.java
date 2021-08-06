4f7a22df7d71a226be571a011666177c8140ebc9
beebeeeboss <simpybadhwar42@gmail.com>
2021-08-06 11:00:35 +0530

    registercommit

--- app/src/main/java/com/pharmacy/atmycare/ui/ResgisterUserFragment.java
+++ app/src/main/java/com/pharmacy/atmycare/ui/ResgisterUserFragment.java
@@ -2,21 +2,32 @@ package com.pharmacy.atmycare.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import com.pharmacy.atmycare.R;
+import com.pharmacy.atmycare.databinding.FragmentLoginBinding;
 
 
 public class ResgisterUserFragment extends Fragment {
+    private FragmentLoginBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_resgister_user, container, false);
+
+
     }
 }
+
+
+
