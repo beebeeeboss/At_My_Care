d26462ff872b9f547397b7f15ae6d1eb2e207c84
Punit <nit9752@gmail.com>
2021-08-21 19:39:25 +0530

    punit00

---
+++ app/src/main/java/com/pharmacy/atmycare/Adapter/UserVideoAdapter.java
@@ -0,0 +1,60 @@
+package com.pharmacy.atmycare.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.pharmacy.atmycare.R;
+import com.pharmacy.atmycare.model.Video_User;
+
+import java.util.List;
+
+public class UserVideoAdapter extends RecyclerView.Adapter<UserVideoAdapter.UserVideoViewHolder> {
+
+    private Context context;
+    private List<Video_User> videoHealthDataList;
+
+    public UserVideoAdapter(Context context, List<Video_User> videoHealthDataList) {
+        this.context = context;
+        this.videoHealthDataList = videoHealthDataList;
+    }
+
+    @NonNull
+    @Override
+    public UserVideoAdapter.UserVideoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_video_cardview_design, parent, false);
+        return new UserVideoAdapter.UserVideoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserVideoAdapter.UserVideoViewHolder holder, int position) {
+       // holder.vvVideoVlog.;
+        holder.vtvSmallTitle.setText(videoHealthDataList.get(position).getSmallTitle());
+        holder.vtvBigTitle.setText(videoHealthDataList.get(position).getBigTitle());
+
+        holder.vtvLikes.setText(videoHealthDataList.get(position).getNoOfLikes()+" Likes");
+    }
+
+    @Override
+    public int getItemCount() { return videoHealthDataList.size();}
+
+    public static class UserVideoViewHolder extends RecyclerView.ViewHolder {
+        private VideoView vvVideoVlog;
+        private TextView vtvSmallTitle , vtvBigTitle, vtvLikes;
+
+        public UserVideoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            vvVideoVlog = itemView.findViewById(R.id.vvVideoBlog);
+            vtvSmallTitle = itemView.findViewById(R.id.vtvSmallTitle);
+            vtvBigTitle = itemView.findViewById(R.id.vtvHealthBlogBigTiltle);
+            vtvLikes = itemView.findViewById(R.id.vtvHealthBlogLikes);
+        }
+    }
+}
