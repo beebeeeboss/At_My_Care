e7e0dd4debfc36be97a5099b35b947aed0c3b199
Punit <nit9752@gmail.com>
2021-08-29 02:28:42 +0530

    medicine layout done(note:button and top view to fix)

--- app/src/main/java/com/pharmacy/atmycare/ui/BuyMedicinesUserFragment.java
+++ app/src/main/java/com/pharmacy/atmycare/ui/BuyMedicinesUserFragment.java
@@ -5,18 +5,26 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.pharmacy.atmycare.Adapter.UserContentAdapter;
 import com.pharmacy.atmycare.R;
 import com.pharmacy.atmycare.databinding.FragmentAccountUserBinding;
 import com.pharmacy.atmycare.databinding.FragmentBuyMedicinesUserBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BuyMedicinesUserFragment extends Fragment {
 
     private FragmentBuyMedicinesUserBinding bindings;
+    private UserContentAdapter adapter;
+    private List<Integer> imgList;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,5 +35,18 @@ public class BuyMedicinesUserFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        imgList = new ArrayList<>();
+        adapter = new UserContentAdapter(getContext(),imgList);
+        bindings.rvMedicineImg.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
+        bindings.rvMedicineImg.setAdapter(adapter);
+
+        imgList.add(R.drawable.book);
+        imgList.add(R.drawable.heart);
+        imgList.add(R.drawable.stethoscope);
+        imgList.add(R.drawable.book);
+
+        adapter= new UserContentAdapter(getContext(),imgList);
+        adapter.notifyDataSetChanged();
+
     }
 }
