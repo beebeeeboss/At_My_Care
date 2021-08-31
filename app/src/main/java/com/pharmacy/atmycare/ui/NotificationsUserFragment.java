45f50a4503a64be78c7a51521a8f9b9cc83b5795
Punit <nit9752@gmail.com>
2021-08-31 10:37:18 +0530

    medicine layout done(note:button and top view to fix),

--- app/src/main/java/com/pharmacy/atmycare/ui/NotificationsUserFragment.java
+++ app/src/main/java/com/pharmacy/atmycare/ui/NotificationsUserFragment.java
@@ -1,5 +1,6 @@
 package com.pharmacy.atmycare.ui;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -20,12 +21,18 @@ public class NotificationsUserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        bindings = FragmentNotificationsUserBinding.inflate(inflater , container , false);
-        return bindings.getRoot();
+        //bindings = FragmentNotificationsUserBinding.inflate(inflater , container , false);
+      //  return bindings.getRoot();
+        return inflater.inflate(R.layout.dilog_fragment,container,false);
+        //return  bindings.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+
     }
-}
+
+    }
