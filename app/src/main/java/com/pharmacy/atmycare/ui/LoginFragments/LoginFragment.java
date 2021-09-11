9133b8e31cf8b4e195d45171ad82439ae8a960eb
ParamSaini <74251396+param-18@users.noreply.github.com>
2021-09-11 09:29:43 +0530

    Now 3 logins are working with no errors

--- app/src/main/java/com/pharmacy/atmycare/ui/LoginFragments/LoginFragment.java
+++ app/src/main/java/com/pharmacy/atmycare/ui/LoginFragments/LoginFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,92 +33,59 @@ import in.aabhasjindal.otptextview.OtpTextView;
 public class LoginFragment extends Fragment {
 
     private FragmentLoginBinding binding;
-    private  OtpTextView otpView;
+
     // use this binding for find view by id purpose
     // like textview as binding.idnamedefinedinresourcefile
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = FragmentLoginBinding.inflate(inflater, container , false);
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
     @Override
-    public void onViewCreated(@NonNull  View view, @Nullable  Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext() , R.array.user_type, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        binding.spLoginType.setAdapter(adapter);
-         otpView = binding.ovReceivedOtp;
-        binding.btnSendOTP.setOnClickListener(v->
-        {
-            if(binding.spLoginType.getSelectedItemPosition() == 0)
-            {
-                Snackbar.make(getView(),"Please Select Your Role First.",Snackbar.LENGTH_LONG).show();
+
+        if (UserLoginFragment.goTo != null){
+            if (UserLoginFragment.goTo.equals("reg")) {
+                Navigation.findNavController(getActivity().findViewById(R.id.fragmentContainerView2)).navigate(LoginFragmentDirections.actionLoginFragmentToResgisterUserFragment());
+            } else if (UserLoginFragment.goTo.equals("dash")) {
+                Navigation.findNavController(getActivity().findViewById(R.id.fragmentContainerView2)).navigate(LoginFragmentDirections.actionLoginFragmentToUserDashboardFragment());
             }
-            else {
-                if (binding.etPhoneNumber.getText().toString().length() < 10) {
-                    binding.tilPhoneNumber.setError("Please Fill Valid Mobile Number");
-                } else {
-                    binding.etPhoneNumber.setEnabled(false);
-                    binding.tilPhoneNumber.setVisibility(View.GONE);
-                    binding.btnSendOTP.setVisibility(View.GONE);
-                    binding.spLoginType.setVisibility(View.GONE);
-                    binding.llOtp.setVisibility(View.VISIBLE);
-                    binding.tvResendOTP.setVisibility(View.VISIBLE);
-                    binding.tvChangeNumber.setVisibility(View.VISIBLE);
-
-                }
-            }
-        });
-
-        otpView.setOtpListener(new OTPListener() {
-            @Override
-            public void onInteractionListener() {
-
-            }
-
-            @Override
-            public void onOTPComplete(String otp) {
-                if (otpView.getOTP().equals("123456")) {
-                    otpView.showSuccess();
-                    switch (binding.spLoginType.getSelectedItemPosition()) {
+        } else {
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.user_type, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            binding.spLoginType.setAdapter(adapter);
+            binding.fragmentContainerView3.setVisibility(View.GONE);
+            binding.spLoginType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                    switch (i) {
                         case 1:
-                            //go to admin
+                            binding.fragmentContainerView3.setVisibility(View.VISIBLE);
+                            Navigation.findNavController(binding.fragmentContainerView3).navigateUp();
+                            Navigation.findNavController(binding.fragmentContainerView3).navigate(UserLoginFragmentDirections.actionUserLoginFragmentToAdminLoginFragment());
                             break;
                         case 2:
-                            //go to ATMx Register Form or Dashboard according to database
+                            binding.fragmentContainerView3.setVisibility(View.VISIBLE);
+                            Navigation.findNavController(binding.fragmentContainerView3).navigateUp();
+                            Navigation.findNavController(binding.fragmentContainerView3).navigate(UserLoginFragmentDirections.actionUserLoginFragmentToATMxLoginFragment());
                             break;
                         case 3:
-                            //go to User Register Form or Dashboard according to database
-                            Navigation.findNavController(getView()).navigate(LoginFragmentDirections.actionLoginFragmentToResgisterUserFragment());
+                            binding.fragmentContainerView3.setVisibility(View.VISIBLE);
+                            Navigation.findNavController(binding.fragmentContainerView3).navigateUp();
+                            Navigation.findNavController(binding.fragmentContainerView3).navigate(UserLoginFragmentDirections.actionUserLoginFragmentSelf());
                             break;
                     }
-                } else {
-                    otpView.showError();
                 }
-            }
-        });
 
-        binding.tvChangeNumber.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                binding.etPhoneNumber.setEnabled(true);
-                binding.spLoginType.setVisibility(View.VISIBLE);
-                binding.tilPhoneNumber.setVisibility(View.VISIBLE);
-                binding.btnSendOTP.setVisibility(View.VISIBLE);
-                binding.llOtp.setVisibility(View.GONE);
-                binding.tvResendOTP.setVisibility(View.GONE);
-                binding.tvChangeNumber.setVisibility(View.GONE);
-            }
-        });
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
 
-        binding.tvResendOTP.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                otpView.setOTP("");
-            }
-        });
+                }
+            });
+        }
     }
 }
