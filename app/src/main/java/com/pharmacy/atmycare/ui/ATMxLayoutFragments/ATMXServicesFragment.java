016275171e0ad6a49022c3f3bb2749163de7961e
Punit <nit9752@gmail.com>
2021-09-29 15:05:15 +0530

    AtmX and Admin login done updatedhjh

--- app/src/main/java/com/pharmacy/atmycare/ui/ATMxLayoutFragments/ATMXServicesFragment.java
+++ app/src/main/java/com/pharmacy/atmycare/ui/ATMxLayoutFragments/ATMXServicesFragment.java
@@ -6,16 +6,34 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.pharmacy.atmycare.Adapter.UserContentAdapter;
+import com.pharmacy.atmycare.Adapter.UserHealthBlogsAdapter;
+import com.pharmacy.atmycare.Adapter.UserVideoAdapter;
 import com.pharmacy.atmycare.R;
 import com.pharmacy.atmycare.databinding.FragmentATMXServicesBinding;
+import com.pharmacy.atmycare.model.HealthBlog_User;
+import com.pharmacy.atmycare.model.Video_User;
+
+import java.util.List;
 
 public class ATMXServicesFragment extends Fragment {
 
     private FragmentATMXServicesBinding binding;
+    private UserContentAdapter mUserContentAdapter;
+    private UserHealthBlogsAdapter mUserHealthBlogsAdapter;
+
+    private UserVideoAdapter mUserVideoAdapter;
+    private List<Integer> imageResourceList;
+    private List<HealthBlog_User> healthDataList;
+
+    private List<Video_User> videoDataList;
+    private CountDownTimer forContent;
+    private int count = 0;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,5 +45,6 @@ public class ATMXServicesFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
     }
 }
